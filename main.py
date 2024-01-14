9d45795916afc2a46f3057494e02ae783d02aac4
Miłosz <101997323+SzramStaR@users.noreply.github.com>
2024-01-14 19:25:05 +0100

    Sort of final version of model

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ import time
 import mediapipe as mp
 from time import sleep
 from tensorflow.keras.preprocessing.image import img_to_array
-
+import collections
+from scipy.spatial.distance import pdist
 
 #Load the model
-model = load_model('My_model.h5')
+model = load_model('My_model_landmarks_final2.h5')
 #Predcitions dictionary
 labels_to_char = {0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',
-7:'H',8:'I',9:'J',10:'K',11:'L',12:'M',13:'N',
-14:'O',15:'P',16:'Q',17:'R',18:'S',19:'T',20:'U',
-21:'V',22:'W',23:'X',24:'Y',25:'Z',26:'del',27:'space',28:'nothing'}
+7:'H',8:'I',9:'K',10:'L',11:'M',12:'N',
+13:'O',14:'P',15:'Q',16:'R',17:'S',18:'T',19:'U',
+20:'V',21:'W',22:'X',23:'Y',24:'Z',25:'space',26:'nothing'}
 
 #Media pipe setup
 mp_hands = mp.solutions.hands
@@ -36,7 +37,9 @@ padding = 50 #Padding so that whole hand fits
 # print(labels_to_char[label])
 
 
-
+last_prediction_time = None
+predictions=[]
+output_text = ""
 video = cv2.VideoCapture(0)
 while True:
     ret, frame = video.read()
@@ -56,31 +59,33 @@ while True:
             y_min = max(0,min([landmark.y for landmark in hand_landmarks.landmark])*frame.shape[0]-padding)
             y_max = min(frame.shape[0],max([landmark.y for landmark in hand_landmarks.landmark])*frame.shape[0]+padding)
 
-            #Crop the hand
-            hand_img = frame[int(y_min):int(y_max),int(x_min):int(x_max)]
-            #Resize the hand for the model
-            hand_img = cv2.resize(hand_img,(64,64))
-            hand_img = hand_img/255
-            hand_img = np.expand_dims(hand_img,axis=0)
-            #Predict the letter
-            # pred = model.predict(hand_img)
-            # label = np.argmax(pred)
+            #Draw hand landmarks
+            mp_drawing.draw_landmarks(frame,hand_landmarks,mp_hands.HAND_CONNECTIONS)
     
-    
-            # print(labels_to_char[label])
-
-            #Draw the bounding box
-            cv2.rectangle(frame,(int(x_min),int(y_min)),(int(x_max),int(y_max)),(0,0,255),2)
-            hand_frame = frame[int(y_min):int(y_max),int(x_min):int(x_max)]
-            hand_frame = cv2.resize(hand_frame,(64,64))
-            hand_frame = hand_frame/255
-            pred = model.predict(np.expand_dims(hand_frame,axis=0))
+            landmarks = np.array([[landmark.x,landmark.y, landmark.z] for landmark in hand_landmarks.landmark])
+            distances = pdist(landmarks,'euclidean')
+            #Normalize landmarks
+            # landmarks = landmarks - np.mean(landmarks,axis=0)
+            #landmarks = landmarks/np.std(landmarks,axis=0)
+            #RESHAPE
+            landmarks = np.reshape(landmarks,(1,21,3))
+            pred = model.predict(distances.reshape(1,-1), verbose=0)
             label = np.argmax(pred)
-            print(labels_to_char[label])
-            # cv2.imshow("Hand",hand_frame)
+            predictions.append(label)
+            last_prediction_time = time.time()
+            if(last_prediction_time is not None and time.time() - last_prediction_time > 2):
+                predictions.clear()
+                output_text+=" "   
+            if len(predictions) == 30:
+                most_common = collections.Counter(predictions).most_common(1)
+                print(labels_to_char[most_common[0][0]])
+                output_text+=labels_to_char[most_common[0][0]]
+                predictions.clear()
+
     cv2.imshow("MediaPipe Hands", frame)
     if cv2.waitKey(5) & 0xFF == 27:
         break
 
+print(output_text)
 video.release()
 cv2.destroyAllWindows()
